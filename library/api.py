d89a2d49d923d55e4e0531259a1127346bfa8ef7
Hemil96 <hemilpatel41196@gmail.com>
2018-02-09 03:14:37 +0530

    UI improved with bootstrap and API updated

--- library/api.py
+++ library/api.py
@@ -5,8 +5,33 @@ import json
 url = "https://coinbin.org/coins" #API URl
 api = requests.get(url).text #Getting html response to text
 dic = json.loads(api) #converting JSON into python dict 
+coin_list = ["temp"]
+for key, value in dic.iteritems():
+    for key in value:
+    	coin_list.append(key)
 
+#Direct Key : value
 def coin(name):
-	coin_data =  dic["coins"][name]
+	name = name.lower()
+	if name not in coin_list:
+		return full_name(name)
+		# return "n/a"
+	else:
+		coin_data =  dic["coins"][name]
 	return coin_data
+#Finding name from value 
+def full_name(name):
+	for key, value in dic.iteritems():
+		for key_inner, value_inner in value.iteritems():
+			if value_inner["name"].lower() == name:
+				print value_inner
+				return value_inner
+				break
+			else:
+				pass
+		return "n/a"
+
+
+
+				
 
--- library/main.py
+++ library/main.py
@@ -13,19 +13,36 @@ def hello_world():
         return render_template('forms/basic_form.html')
         
     elif request.method == 'POST': #handling POST request
-        return redirect("/detail/"+request.form['coin_name'], code=302) #redirecting to other url
+        api_res = api.coin(request.form['coin_name']) #fetching API response 
+        print api_res # Tesing perpose [CLI]
+
+        #Checking for keyerror/coin_name
+        if api_res == "n/a":
+            return render_template('forms/404.html')
+
+        else:
+            kwargs = {
+                'f_name' : api_res["name"],
+                'btc_rate' : api_res["btc"],
+                'usd_rate' : api_res["usd"],
+                'rank' : api_res["rank"],
+                'ticker' : api_res["ticker"]
+            }
+            return render_template('forms/basic_form_result.html', **kwargs)
+            # return redirect("/detail/"+request.form['coin_name'], code=302) #redirecting to other url
 
 
 
-@app.route('/detail/<coin_name>')
-def coinroute(coin_name):
-    api_res = api.coin(coin_name) #fetching API response 
-    print api_res # Tesing perpose [CLI]
-    kwargs = {
-        'f_name' : api_res["name"],
-        'btc_rate' : api_res["btc"],
-        'usd_rate' : api_res["usd"],
-        'rank' : api_res["rank"],
-        'ticker' : api_res["ticker"]
-    }
-    return render_template('forms/basic_form_result.html', **kwargs)
+
+# @app.route('/detail/<coin_name>')
+# def coinroute(coin_name):
+#     api_res = api.coin(coin_name) #fetching API response 
+#     print api_res # Tesing perpose [CLI]
+#     kwargs = {
+#         'f_name' : api_res["name"],
+#         'btc_rate' : api_res["btc"],
+#         'usd_rate' : api_res["usd"],
+#         'rank' : api_res["rank"],
+#         'ticker' : api_res["ticker"]
+#     }
+#     return render_template('forms/basic_form_result.html', **kwargs)
