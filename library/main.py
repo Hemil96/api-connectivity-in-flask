5d79765a70ada7427c4e726acae4486e74fd46d8
Hemil96 <hemilpatel41196@gmail.com>
2018-02-12 16:15:44 +0530

    necessary code removed

--- library/main.py
+++ library/main.py
@@ -11,9 +11,9 @@ def hello_world():
 
     if request.method == 'GET': #handling GET request
         return render_template('forms/basic_form.html')
-        
+
     elif request.method == 'POST': #handling POST request
-        api_res = api.coin(request.form['coin_name']) #fetching API response 
+        api_res = api.coin(request.form['coin_name']) #fetching API response
         print api_res # Tesing perpose [CLI]
 
         #Checking for keyerror/coin_name
@@ -30,19 +30,3 @@ def hello_world():
             }
             return render_template('forms/basic_form_result.html', **kwargs)
             # return redirect("/detail/"+request.form['coin_name'], code=302) #redirecting to other url
-
-
-
-
-# @app.route('/detail/<coin_name>')
-# def coinroute(coin_name):
-#     api_res = api.coin(coin_name) #fetching API response 
-#     print api_res # Tesing perpose [CLI]
-#     kwargs = {
-#         'f_name' : api_res["name"],
-#         'btc_rate' : api_res["btc"],
-#         'usd_rate' : api_res["usd"],
-#         'rank' : api_res["rank"],
-#         'ticker' : api_res["ticker"]
-#     }
-#     return render_template('forms/basic_form_result.html', **kwargs)
